2679eba92a6f8a480f03cd26b20e770778276041
TariqMehmood1004 <116074810+TariqMehmood1004@users.noreply.github.com>
2023-04-11 15:30:03 +0500

    updating drawer controller

--- lib/Areas/Controllers/Drawer_controller/drawer_controller.dart
+++ lib/Areas/Controllers/Drawer_controller/drawer_controller.dart
@@ -18,95 +18,157 @@ class _DrawerControllerScreenState extends State<DrawerControllerScreen> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: AppColors.color6,
+        color: AppColors.color1,
         borderRadius: BorderRadius.circular(0.0),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 20),
-            decoration: BoxDecoration(
-              color: AppColors.tranparent,
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const CircleAvatar(
-                  radius: 17,
-                  backgroundImage: AssetImage('images/tm.jpg'),
-                ),
-                const SizedBox(
-                  width: 10.0,
-                ),
-                TextWidget(
-                  "Tariq Mehmood".toUpperCase(),
-                  16.0,
-                  AppColors.black,
-                  FontWeight.w600,
-                  0,
-                  TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: 260,
-              height: 400,
-              color: AppColors.tranparent,
-              margin: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              margin:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+              decoration: BoxDecoration(
+                color: AppColors.tranparent,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Container(
-                    width: double.infinity,
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 5.0, vertical: 10.0),
-                    decoration: BoxDecoration(
+                  const CircleAvatar(
+                    radius: 17,
+                    backgroundImage: AssetImage('images/tm.jpg'),
+                  ),
+                  const SizedBox(
+                    width: 13.0,
+                  ),
+                  TextWidget(
+                    "Tariq Mehmood".toUpperCase(),
+                    16.0,
+                    AppColors.black,
+                    FontWeight.w500,
+                    0,
+                    TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: 260,
+                height: 400,
+                color: AppColors.tranparent,
+                margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 5.0, vertical: 10.0),
+                      decoration: BoxDecoration(
                         border: Border(
-                      bottom: BorderSide(width: .4, color: AppColors.white),
-                    )),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5.0),
-                      child: Text(
-                        'Question Type'.toUpperCase(),
-                        style: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                            color: AppColors.black,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w500,
-                            letterSpacing: 1,
+                          bottom: BorderSide(width: .4, color: AppColors.white),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5.0),
+                        child: Text(
+                          'Question Type'.toUpperCase(),
+                          style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                              color: AppColors.black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // here is coming a issue...
-                      setState(
-                        () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const MCQSController(),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // here is coming a issue...
+                        setState(
+                          () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MCQSController(),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      child: Material(
+                        color: Colors.transparent,
+                        child: Container(
+                          padding: const EdgeInsets.all(5.0),
+                          margin: const EdgeInsets.symmetric(vertical: 3.0),
+                          decoration: BoxDecoration(
+                            color: AppColors.tranparent,
+                            border: Border(
+                              bottom: BorderSide(
+                                width: .1,
+                                color: AppColors.bluePink2,
+                              ),
                             ),
-                          );
-                        },
-                      );
-                    },
-                    child: Material(
-                      color: Colors.transparent,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Image(
+                                width: 20,
+                                height: 20,
+                                image: AssetImage('images/mcqs.png'),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              TextWidget(
+                                "MCQs",
+                                12.0,
+                                AppColors.black,
+                                FontWeight.w500,
+                                0,
+                                TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(
+                          () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const TrueFalseController(),
+                              ),
+                            );
+                          },
+                        );
+                      },
                       child: Container(
                         padding: const EdgeInsets.all(5.0),
-                        color: AppColors.tranparent,
-                        margin: const EdgeInsets.symmetric(vertical: 3.0),
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        decoration: BoxDecoration(
+                          color: AppColors.tranparent,
+                          border: Border(
+                            bottom: BorderSide(
+                              width: .1,
+                              color: AppColors.bluePink2,
+                            ),
+                          ),
+                        ),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.center,
@@ -114,13 +176,13 @@ class _DrawerControllerScreenState extends State<DrawerControllerScreen> {
                             const Image(
                               width: 20,
                               height: 20,
-                              image: AssetImage('images/mcqs.png'),
+                              image: AssetImage('images/mcqs2.png'),
                             ),
                             const SizedBox(
                               width: 10,
                             ),
                             TextWidget(
-                              "MCQs",
+                              "True/False",
                               12.0,
                               AppColors.black,
                               FontWeight.w500,
@@ -131,58 +193,12 @@ class _DrawerControllerScreenState extends State<DrawerControllerScreen> {
                         ),
                       ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(
-                        () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const TrueFalseController(),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(5.0),
-                      color: AppColors.tranparent,
-                      margin: const EdgeInsets.symmetric(vertical: 3.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Image(
-                            width: 20,
-                            height: 20,
-                            image: AssetImage('images/mcqs2.png'),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          TextWidget(
-                            "True/False",
-                            12.0,
-                            AppColors.black,
-                            FontWeight.w500,
-                            0,
-                            TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            margin:
-                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
-            child: const Text('dd'),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
